a246222a5c6800949249547328f919e89b172528
2778382859@qq.com <a739909147>
2014-09-29 14:55:47 +0800

    init

---
+++ UiFrist/src/app/MyApp.lua
@@ -0,0 +1,17 @@
+
+require("config")
+require("framework.init")
+
+local MyApp = class("MyApp", cc.mvc.AppBase)
+
+function MyApp:ctor()
+    MyApp.super.ctor(self)
+end
+
+function MyApp:run()
+    cc.FileUtils:getInstance():addSearchPath("res/")
+    display.addSpriteFrames(GAME_TEXTURE_DATA_FILENAME, GAME_TEXTURE_IMAGE_FILENAME)
+    self:enterScene("MainScene")
+end
+
+return MyApp
